a91e7d7fdf3a186472a419aa8a86540b7ee6efa2
Adiatmaja <johannes.baptista@ti.ukdw.ac.id>
2022-06-16 14:51:23 +0700

    Delete File

--- MiniProject2/login.js
+++
@@ -1,9 +0,0 @@
-var inpUsername = document.getElementById("username");
-var inpPassword = document.getElementById("password");
-
-function validationLogin(){
-    if(inpUsername === "" || inpPassword === ""){
-        document.getElementsByClassName("alert").innerHTML = "Username atau Password tidak boleh kosong";
-        event.preventDefault();
-    }
-}
