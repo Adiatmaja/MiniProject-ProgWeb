04888a2e973cd1fc11d62445e45580bf80fa2812
Adiatmaja <johannes.baptista@ti.ukdw.ac.id>
2022-06-16 14:39:42 +0700

    Update File

--- MiniProject2/data.php
+++ MiniProject2/data.php
@@ -11,14 +11,15 @@ if(!isset($_SESSION["username"])){
 
 $user=$_SESSION["username"];
 
-// Pre fill Link Video Ketika Update msh error
 
 $sqlInstruktur = "SELECT * FROM instruktur";
 $resultInstruktur = mysqli_query($conn,$sqlInstruktur);
 
 $targetFolder = "assets/";
 
+$id = null;
 if (isset($_GET["id"])){
+    $id = $_GET["id"];
     $IdOlahraga = $_GET["id"];
 
     $sql="
@@ -34,7 +35,10 @@ if (isset($_GET["id"])){
     ";
     $result = mysqli_query($conn,$sql);
     $data = mysqli_fetch_assoc($result);
+<<<<<<< Updated upstream
     $id=$data["IdOlahraga"];
+=======
+>>>>>>> Stashed changes
     $NamaOlahraga = $data["NamaOlahraga"];
     $NamaTipe = $data["TipeOlahraga"];
     $NamaLevel = $data["Level"];
@@ -55,6 +59,7 @@ if (isset($_GET["id"])){
 }
 
 if ($_POST){
+<<<<<<< Updated upstream
     if ($_POST["IdOlahraga"]!=null) {
         $NamaOlahraga = $_POST["NamaOlahraga"];
         $IdTipe = $_POST["IdTipe"];
@@ -108,6 +113,83 @@ if ($_POST){
                             <script>
                                 alert('Data Gagal Diupdate');
                                 document.location.href = 'dashboard.php';
+=======
+    $IdTipe = $_POST["IdTipe"];
+    $IdLevel = $_POST["IdLevel"];
+    $IdInstruktur = $_POST["IdInstruktur"];
+    if ($IdTipe != null && $IdLevel != null && $IdInstruktur != null){
+        if ($_POST["IdOlahraga"] != null) {
+            $IdOlahraga = $_POST["IdOlahraga"];
+            $NamaOlahraga = $_POST["NamaOlahraga"];
+            $Deskripsi = $_POST["Deskripsi"];
+            $Peralatan = $_POST["Peralatan"];
+            $LinkVideo = $_POST["LinkVideo"];
+            $Durasi = $_POST["Durasi"];
+            
+
+            $sqlSelectVideoGambar = "SELECT IdVideo, IdImage FROM olahraga WHERE IdOlahraga = '".$IdOlahraga."'";
+            $resultSelectVideoGambar = mysqli_query($conn,$sqlSelectVideoGambar);
+            $dataSelectVideoGambar = mysqli_fetch_assoc($resultSelectVideoGambar);
+            $IdVideo = $dataSelectVideoGambar["IdVideo"];
+            $IdImage = $dataSelectVideoGambar["IdImage"];
+            
+            // Cek Format File
+            $extension = pathinfo($_FILES["Image"]["name"], PATHINFO_EXTENSION);
+            $extension = strtolower($extension);
+            if($extension == "jpg" || $extension == "jpeg" || $extension == "png"){
+                // Update Tabel Video
+                $sqlVideo = "UPDATE video SET 
+                            LinkVideo = '".$LinkVideo."', 
+                            Durasi = '".$Durasi."' 
+                            WHERE IdVideo = '".$IdVideo."'";
+                if (mysqli_query($conn, $sqlVideo)) {
+                    // Update Tabel Image
+                    if (isset($_FILES)){
+                        $temp = explode(".", $_FILES['Image']['name']);
+                        $FileName = round(microtime(true)) . '.' . end($temp); // Rename File berdasarkan waktu sekarang
+                        $path = $targetFolder.$FileName;
+                        if(move_uploaded_file($_FILES['Image']['tmp_name'], $path)){
+                            $sqlGambar = "UPDATE image SET 
+                                        ImagePath = '".$path."' 
+                                        WHERE IdImage = '".$IdImage."'";
+                            if (mysqli_query($conn, $sqlGambar)) {
+                                // Update Tabel Olahraga
+                                $Video = "SELECT IdVideo FROM video WHERE LinkVideo = '".$LinkVideo."'";
+                                $resultVideo = mysqli_query($conn, $Video);
+                                $dataVideo = mysqli_fetch_assoc($resultVideo);
+                                $Image = "SELECT IdImage FROM image WHERE ImagePath = '".$path."'";
+                                $resultImage = mysqli_query($conn, $Image);
+                                $dataImage = mysqli_fetch_assoc($resultImage);
+                                $sqlOlahraga = "UPDATE olahraga SET 
+                                                NamaOlahraga = '".$NamaOlahraga."', 
+                                                IdTipe = '".$IdTipe."', 
+                                                IdLevel = '".$IdLevel."', 
+                                                IdInstruktur = '".$IdInstruktur."', 
+                                                Deskripsi = '".$Deskripsi."', 
+                                                Peralatan = '".$Peralatan."', 
+                                                IdVideo = '".$dataVideo["IdVideo"]."', 
+                                                IdImage = '".$dataImage["IdImage"]."' 
+                                                WHERE IdOlahraga = '".$IdOlahraga."'";
+                                if (mysqli_query($conn, $sqlOlahraga)) {
+                                    echo "
+                                    <script>
+                                        alert('Data Berhasil Diupdate');
+                                        document.location.href = 'dashboard.php';
+                                    </script>
+                                    ";
+                                } else {
+                                    echo "
+                                    <script>
+                                        alert('Data Gagal Diupdate');
+                                    </script>
+                                    ";
+                                }
+                            }
+                        } else {
+                            echo "
+                            <script>
+                                alert('Gagal Update Gambar');
+>>>>>>> Stashed changes
                             </script>
                             ";
                         }
@@ -115,12 +197,23 @@ if ($_POST){
                 } else {
                     echo "
                     <script>
+<<<<<<< Updated upstream
                         alert('Gagal Update Gambar');
                         document.location.href = 'dashboard.php';
+=======
+                        alert('Data Video Gagal Diupdate');
+>>>>>>> Stashed changes
                     </script>
                     ";
                 }
+            } else {
+                echo "
+                <script>
+                    alert('Format File Tidak Sesuai');
+                </script>
+                ";
             }
+<<<<<<< Updated upstream
         } else {
             echo "
             <script>
@@ -168,6 +261,57 @@ if ($_POST){
                             <script>
                                 alert('Data Gagal Ditambah');
                                 document.location.href = 'dashboard.php';
+=======
+        }else {
+            $NamaOlahraga = $_POST["NamaOlahraga"];
+            $Deskripsi = $_POST["Deskripsi"];
+            $Peralatan = $_POST["Peralatan"];
+            $LinkVideo = $_POST["LinkVideo"];
+            $Durasi = $_POST["Durasi"];
+            
+            // Cek Format File
+            $extension = pathinfo($_FILES['Image']['name'], PATHINFO_EXTENSION);
+            $extension = strtolower($extension);
+            if ($extension == "jpg" || $extension == "png" || $extension == "jpeg") {
+                // Insert Tabel Video
+                $sqlVideo = "INSERT INTO video VALUES ('', '".$LinkVideo."', '".$Durasi."')";
+                if (mysqli_query($conn, $sqlVideo)) {
+                    // Insert Tabel Image
+                    if (isset($_FILES)){
+                        $temp = explode(".", $_FILES['Image']['name']);
+                        $FileName = round(microtime(true)) . '.' . end($temp); // Rename File berdasarkan waktu sekarang
+                        $path = $targetFolder.$FileName;
+                        if(move_uploaded_file($_FILES['Image']['tmp_name'], $path)){
+                            $sqlGambar = "INSERT INTO image VALUES('', '".$path."')";
+                            if (mysqli_query($conn, $sqlGambar)) {
+                                // Insert Tabel Olahraga
+                                $Video = "SELECT IdVideo FROM video WHERE LinkVideo = '".$LinkVideo."'";
+                                $resultVideo = mysqli_query($conn, $Video);
+                                $dataVideo = mysqli_fetch_assoc($resultVideo);
+                                $Image = "SELECT IdImage FROM image WHERE ImagePath = '".$path."'";
+                                $resultImage = mysqli_query($conn, $Image);
+                                $dataImage = mysqli_fetch_assoc($resultImage);
+                                $sqlOlahraga = "INSERT INTO olahraga VALUES ('', '".$NamaOlahraga."', '".$IdTipe."', '".$IdLevel."', '".$IdInstruktur."', '".$Deskripsi."', '".$Peralatan."', '".$dataVideo["IdVideo"]."', '".$dataImage["IdImage"]."')";
+                                if (mysqli_query($conn, $sqlOlahraga)) {
+                                    echo "
+                                    <script>
+                                        alert('Data Berhasil Ditambah');
+                                        document.location.href = 'dashboard.php';
+                                    </script>
+                                    ";
+                                } else {
+                                    echo "
+                                    <script>
+                                        alert('Data Gagal Ditambah');
+                                    </script>
+                                    ";
+                                }
+                            }
+                        } else {
+                            echo "
+                            <script>
+                                alert('Gagal Upload Gambar');
+>>>>>>> Stashed changes
                             </script>
                             ";
                         }
@@ -175,12 +319,23 @@ if ($_POST){
                 } else {
                     echo "
                     <script>
+<<<<<<< Updated upstream
                         alert('Gagal Upload Gambar');
                         document.location.href = 'dashboard.php';
+=======
+                        alert('Data Video Gagal Ditambah');
+>>>>>>> Stashed changes
                     </script>
                     ";
                 }
+            } else {
+                echo "
+                <script>
+                    alert('Format File Gambar Tidak Sesuai');
+                </script>
+                ";
             }
+<<<<<<< Updated upstream
         } else {
             echo "
             <script>
@@ -190,6 +345,16 @@ if ($_POST){
             ";
         }
     } 
+=======
+        }
+    } else {
+        echo "
+        <script>
+            alert('Data Tidak Boleh Kosong');
+        </script>
+        ";
+    }
+>>>>>>> Stashed changes
 }
 
 ?>
@@ -205,7 +370,6 @@ if ($_POST){
 </head>
 <body>
     <header>
-        <!-- <iframe src=""></iframe> -->
         <div class="header">
             <div class="header-kiri">
                 fit.now
@@ -229,7 +393,11 @@ if ($_POST){
     <a class="back" href="dashboard.php">Kembali</a>
     <form class="formdata" action="data.php" method="post" enctype="multipart/form-data">
         <table>
+<<<<<<< Updated upstream
             <input type="hidden" name="IdOlahraga" value="<?php if($IdOlahraga!=0) {echo $Id;}?>">
+=======
+            <input type="hidden" name="IdOlahraga" value="<?php if($id!=0) {echo $IdOlahraga;}?>">
+>>>>>>> Stashed changes
             <tr>
                 <td>Nama Olahraga</td>
                 <td><input type="text" name="NamaOlahraga" value="<?= $NamaOlahraga ?>" required></td>
@@ -237,19 +405,19 @@ if ($_POST){
             <tr>
                 <td>Tipe Olahraga</td>
                 <td><select name="IdTipe">
-                    <option value=""></option>
-                    <option value="1" <?php if($NamaTipe == 'Yoga'){echo " selected =\"selected\"";} ?>>Yoga</option>
-                    <option value="2" <?php if($NamaTipe == 'HIIT'){echo " selected =\"selected\"";} ?>>HIIT</option>
-                    <option value="3" <?php if($NamaTipe == 'Cardio'){echo " selected =\"selected\"";} ?>>Cardio</option>
+                    <option id="Tipe" value=""></option>
+                    <option id="Tipe" value="1" <?php if($NamaTipe == 'Yoga'){echo " selected =\"selected\"";} ?>>Yoga</option>
+                    <option id="Tipe" value="2" <?php if($NamaTipe == 'HIIT'){echo " selected =\"selected\"";} ?>>HIIT</option>
+                    <option id="Tipe" value="3" <?php if($NamaTipe == 'Cardio'){echo " selected =\"selected\"";} ?>>Cardio</option>
                 </select></td>
             </tr>
             <tr>
                 <td>Level Olahraga</td>
                 <td><select name="IdLevel">
-                    <option value=""></option>
-                    <option value="1" <?php if($NamaLevel == 'Beginner'){echo " selected =\"selected\"";} ?>>Beginner</option>
-                    <option value="2" <?php if($NamaLevel == 'Intermediate'){echo " selected =\"selected\"";} ?>>Intermediate</option>
-                    <option value="3" <?php if($NamaLevel == 'Advanced'){echo " selected =\"selected\"";} ?>>Advanced</option>
+                    <option id="Level" value=""></option>
+                    <option id="Level" value="1" <?php if($NamaLevel == 'Beginner'){echo " selected =\"selected\"";} ?>>Beginner</option>
+                    <option id="Level" value="2" <?php if($NamaLevel == 'Intermediate'){echo " selected =\"selected\"";} ?>>Intermediate</option>
+                    <option id="Level" value="3" <?php if($NamaLevel == 'Advanced'){echo " selected =\"selected\"";} ?>>Advanced</option>
                 </select></td>
             </tr>
             <tr>
@@ -257,7 +425,7 @@ if ($_POST){
                 <td><select name="IdInstruktur">
                     <option value=""></option>
                     <?php while($rowInstruktur = mysqli_fetch_row($resultInstruktur)) {
-                        echo "<option value = '{$rowInstruktur[0]}'";
+                        echo "<option id='Instruktur' value = '{$rowInstruktur[0]}'";
                         if($rowInstruktur[0] == $IdInstruktur){
                             echo " selected =\"selected\"";
                         }
@@ -274,22 +442,53 @@ if ($_POST){
                 <td><input type="text" name="Peralatan" value="<?= $Peralatan ?>"></td>
             </tr>
             <tr>
+<<<<<<< Updated upstream
                 <td>Link Video</td>
                 <td><input type="text" name="LinkVideo" value='<?= $IdVideo ?>' required></td>
+=======
+                <td>Link Video (Embed)</td>
+                <td><input type="text" name="LinkVideo" id="Link" value='<?= $IdVideo ?>' required></td>
+>>>>>>> Stashed changes
             </tr>
             <tr>
                 <td>Durasi Video</td>
-                <td><input type="text" name="Durasi" value="<?= $Durasi ?>" required></td>
+                <td><input type="number" name="Durasi" value="<?= $Durasi ?>" required></td>
             </tr>
             <tr>
+                <td>Gambar Olahraga </td>
+                <td rowspan="2"><input type="file" name="Image" value="" required></td>
+            </tr>
+            <tr>
+<<<<<<< Updated upstream
                 <td>Gambar Olahraga</td>
                 <td><input type="file" name="Image" value="" required></td>
+=======
+                <td>(JPG / JPEG / PNG)</td>
+                <td></td>
+            </tr>
+            <tr class="alert">
+                <td class="alert" id="alert" colspan="2"></td>
+                <td></td>
+>>>>>>> Stashed changes
             </tr>
             <tr>
-                <td class="submit" colspan="2"><input class="button" type="submit" class="submit" value="submit"></td>
+                <td class="submit" colspan="2"><input class="button" type="submit" class="submit" value="submit" onclick="validation()"></td>
             </tr>
         </table>
     </form>
+
+    <script type="text/javascript">
+        var inpLink = document.getElementById('Link');
+        var regex = /embed/;
+
+        function validation() {
+            document.getElementById('alert').innerHTML = "";
+            if(!regex.test(inpLink.value)) {
+                document.getElementById('alert').innerHTML = "Mohon gunakan fitur embed pada tombol share video";
+                event.preventDefault();
+            }
+        }
+    </script>
 </body>
 <footer>
         <p>© All Copyright goes to its rightful owner</p>
